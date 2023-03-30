0cd72188bea3d4166f97d5b786133bbd4fa09f40
Dilay Kirisci <isik.kirisci@gmail.com>
2023-03-30 17:08:44 +0300

    final

--- src/App.js
+++ src/App.js
@@ -1,15 +1,43 @@
 import React from "react";
+import axios from "axios";
 import "./App.css";
+import { useEffect, useState } from "react";
+import Component from "./Component.js";
 
 function App() {
-  return (
-    <div className="App">
-      <p>
-        NASA uygulamasını yapmak için README.md dosyasıdaki talimatları takip edin
-		İyi eğlenceler! <span role="img" aria-label='go!'>🚀</span>!
-      </p>
-    </div>
-  );
+	const [apodData, setApodData] = useState();
+	const [datePicker, setDatePicker] = useState(
+		new Date("2022-08-21").toISOString().slice(0, 10)
+	);
+
+	useEffect(() => {
+		// Optionally the request above could also be done as
+		axios
+			.get("https://api.nasa.gov/planetary/apod", {
+				params: {
+					api_key: "Hyt2W8QRMjYjiAKj9vcD2e6mhFfAIesZVgBi29iM",
+					date: datePicker,
+				},
+			})
+			.then(function(response) {
+				console.log(response);
+				setApodData(response.data);
+			})
+			.catch(function(error) {
+				console.log(error);
+			})
+			.finally(function() {});
+	}, [datePicker]);
+
+	return (
+		<div className="App">
+			<Component
+				data={apodData}
+				dateChange={setDatePicker}
+				currentDate={datePicker}
+			/>
+		</div>
+	);
 }
 
 export default App;
